817fbbe1e136f1a7cf5846609e49b8b592b3fd60
chane81 <chane81@naver.com>
2023-04-15 05:42:53 +0900

    refactor: 필요없는 코드 제거

--- iac/aws/modules/eks/providers.tf
+++ iac/aws/modules/eks/providers.tf
@@ -1,21 +1,7 @@
-# provider "kubectl" {
-#   host                   = data.aws_eks_cluster.cluster.endpoint
-#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-#   token                  = data.aws_eks_cluster_auth.cluster_auth.token
-#   load_config_file       = false
-# }
-
-
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.cluster_auth.token
-  # exec {
-  #   api_version = "client.authentication.k8s.io/v1beta1"
-  #   command     = "aws"
-  #   # This requires the awscli to be installed locally where Terraform is executed
-  #   args = ["eks", "get-token", "--cluster-name", local.eks_cluster_name]
-  # }
 
   experiments {
     manifest_resource = true
